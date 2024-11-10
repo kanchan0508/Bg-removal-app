b067f7798ce388b275c00e50ab8248486f361794
kanchan gobari <gobarikanchan53@gmail.com>
2024-11-10 21:13:12 +0530

    created navbar and header

---
+++ src/pages/BuyCredit.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BuyCredit = () => {
+  return (
+    <div>
+      result
+    </div>
+  )
+}
+
+export default BuyCredit
---
+++ src/pages/Result.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Result = () => {
+  return (
+    <div>
+      resultresult
+    </div>
+  )
+}
+
+export default Result
