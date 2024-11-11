e4ee3acc013a40965a5f995de2c0f72e337c9548
kanchan gobari <gobarikanchan53@gmail.com>
2024-11-11 21:03:06 +0530

    Created home page and result page

--- src/pages/BuyCredit.jsx
+++ src/pages/BuyCredit.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import { assets, plans } from "../assets/assets";
 
 const BuyCredit = () => {
   return (
-    <div>
-      result
+    <div className="min-h-[80vh] text-center pt-14 mb-10">
+      <button className="border border-gray-400 px-10 py-2 rounded-full mb-6">Our Plans</button>
+      <h1 className="text-center text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold bg-gradient-to-r from-gray-900 to-gray-400 bg-clip-text text-transparent mb-6 sm:mb-10 ">Choose the plan that's right for you </h1>
+      <div>
+        {plans.map((item, index) => (
+          <div key={index}>
+            <img src={assets.logo_icon} alt="" />
+            <p>{item.id}</p>
+            <p>{item.desc}</p>
+            <p>
+              <span>{item.price}</span>/ {item.credits} credits
+            </p>
+            <button>Purchase</button>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BuyCredit
+export default BuyCredit;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Header from '../Components/Header'
 import Steps from '../Components/Steps'
+import BgSlider from '../Components/BgSlider'
+import Testimonials from '../Components/Testimonials'
+import Upload from '../Components/Upload'
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Steps />
+      <BgSlider />
+      <Testimonials />
+      <Upload />
+      
     </div>
   )
 }
