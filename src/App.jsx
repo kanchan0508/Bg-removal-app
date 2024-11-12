e0352e1517a57e0ab767b5264b9bf907c8319c4e
kanchan gobari <gobarikanchan53@gmail.com>
2024-11-12 21:09:45 +0530

    Completed ui section

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,12 @@ import BuyCredit from './pages/BuyCredit'
 import Navbar from './Components/Navbar'
 import Footer from './Components/Footer'
 
+
 const App = () => {
   return (
     <div className='min-h-screen bg-slate-50'>
       <Navbar />
-
+  
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/result' element={<Result />} />
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { assets } from "../assets/assets";
 import { Link } from "react-router-dom";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 const Navbar = () => {
+  const {openSignIn} = useClerk()
+  const {isSignedIn, user} = useUser()
   return (
     <div className="flex justify-between items-center mx-4 my-3 lg:mx-44">
       <Link to='/' > <img className="w-32 sm:w-44" src={assets.logo} alt="" /></Link>
-      <button className="bg-zinc-800 text-white flex items-center gap-4 px-4 py-2 sm:py-3 sm:px-8 text-sm rounded-full">
+      {
+        isSignedIn ? <div> <UserButton /> </div> : <button onClick={()=>openSignIn({})} className="bg-zinc-800 text-white flex items-center gap-4 px-4 py-2 sm:py-3 sm:px-8 text-sm rounded-full">
         Get Started{" "}
         <img className="w-3 sm:w-4" src={assets.arrow_icon} alt="" />{" "}
       </button>
+      }
+      
     </div>
   );
 };
